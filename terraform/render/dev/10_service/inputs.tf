20e5e31e4dcf717f97685d01831d402b1237d174
Enzo Tang <enzotang@live.fr>
2025-04-03 16:13:48 +0200

    [DEV] Apply

---
+++ terraform/render/dev/10_service/inputs.tf
@@ -0,0 +1,10 @@
+variable "front_registry_url" {
+    type = string
+}
+variable "back_registry_url" {
+    type = string
+}
+
+variable "registry_credential_id" {
+    type = string
+}
---
+++ terraform/render/dev/10_service/main.tf
@@ -0,0 +1,25 @@
+resource "render_web_service" "front" {
+    name = "front"
+    plan = "starter"
+    region = "frankfurt"
+    runtime_source = {
+        image = {
+            image_url = var.front_registry_url
+            registry_credential_id = var.registry_credential_id
+            tag = "latest"
+        }
+    }
+}
+
+resource "render_web_service" "back" {
+    name = "back"
+    plan = "starter"
+    region = "frankfurt"
+    runtime_source = {
+        image = {
+            image_url = var.back_registry_url
+            registry_credential_id = var.registry_credential_id
+            tag = "latest"
+        }
+    }
+}
